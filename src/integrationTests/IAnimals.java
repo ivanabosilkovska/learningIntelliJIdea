b5c481cabb9ac9b6c2b4f5a011d98d32db82f307
Ivana <bosilkovska.ivana@gmail.com>
2022-09-19 20:30:25 +0200

    my first commit

---
+++ src/integrationTests/IAnimals.java
@@ -0,0 +1,5 @@
+package integrationTests;
+
+public interface IAnimals {
+
+}
---
+++ src/integrationTests/LoginTests.java
@@ -0,0 +1,47 @@
+package integrationTests;
+
+public class LoginTests {
+
+    public int getAge() {
+        return age;
+    }
+
+    public LoginTests(int age) {
+        this.age = age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        sayGoodBye("Boban");
+        sayGoodBye("Bojan");
+
+        boolean flag = true;
+        if (flag){
+            System.out.println("Value of flag is true");
+        }else {
+            System.out.println("Value of flag is false");
+        }
+    }
+
+    private int age = 5;
+
+
+    // ovoj metod printa good bye i vrednosta na promenlivata
+    /*
+    kda;lk
+    ajdl
+    sdjsaoi
+    ojaoi
+    sokao
+     */
+    public static void sayGoodBye(String name){
+        // TODO: Add support for multiple users
+        // TODO: To be run once BUG-155 is fixed
+         System.out.println("Good bye " +name);
+    }
+}
