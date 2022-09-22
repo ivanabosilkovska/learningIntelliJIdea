29553ae1f7d546df465edd7a3f15e09542547052
Ivana <bosilkovska.ivana@gmail.com>
2022-09-22 18:52:18 +0200

    updated Test class

--- src/integrationTests/Test.java
+++ src/integrationTests/Test.java
@@ -3,5 +3,9 @@ package integrationTests;
 public class Test {
     public static void main(String[] args) {
         System.out.println("test");
+
+        System.out.println("Test");
+
+        System.out.println("Learning GIT");
     }
 }
